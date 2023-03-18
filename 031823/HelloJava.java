bef4b0bf896952dc83441de58fbeddaf4fe349d0
JinChoi <jinhunchoi94@gmail.com>
2023-03-18 15:06:42 -0500

    firstday learning Java

---
+++ 031823/HelloJava.java
@@ -0,0 +1,17 @@
+//  March 18 2023 by Jin Choi
+
+// Code must be writting in a class
+// Class keyword, class name
+// Class name must always match File name*****
+// main() is Entry point. Java starts running the program here
+
+public class HelloJava {
+
+    public static void main(String[] args) {
+        System.out.println("Hello Java");
+        // Code is case-sensitive
+        // println() prints message to console
+        // Semi-colon means end of statement. MUST NEED
+    }
+
+}
--- README.md
+++ README.md
@@ -1,1 +1,3 @@
 # Java_Learnings
+
+This is for keeping track of my learnings on Java
