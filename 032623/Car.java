53d70341caa4067b8d6040f2ea6a70c09abf4311
JinChoi <jinhunchoi94@gmail.com>
2023-03-27 16:16:47 -0500

    getters and setters

--- 032623/Car.java
+++ 032623/Car.java
@@ -1,18 +1,42 @@
 public class Car {
 //    crete attributes and methods
 
-//    Attributes
-    public int modelYear;
-    public String color;
+    // Attributes
+    private int modelYear;
+    private String color;
 
-//    Methods
-    public void drive(){
-        System.out.println("In Drive mode");
+    // Encapsulation
+
+// Getter
+    public int getModelYear(){
+        return modelYear;
     }
 
-//    Constructor
+    public String getColor() {
+        return this.color;
+    }
+
+// Setter
+    public void setModelYear(int modelYear) {
+        this.modelYear = modelYear;
+    }
+
+    public void setColor(String newColor) {
+        this.color = newColor;
+    }
+
+// Constructor
     public Car(String color, int modelYear){
         this.color = color;
         this.modelYear = modelYear;
+    }
+
+    static void sayHello(){
+        System.out.println("Hello!");
+    }
+
+// Methods
+    public void drive(){
+        System.out.println("In Drive mode");
     }
 }
